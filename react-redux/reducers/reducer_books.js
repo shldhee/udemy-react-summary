8cba544efa1e9f2a992f3568da7472d03326a751
LEEDUCKHEE <shldhee@yahoo.co.kr>
2017-12-21 01:00:49 +0900

    리듀서 내용 추가

---
+++ react-redux/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts'},
+    { title: 'Javascript: ES6'},
+    { title: 'Javascript: React'},
+    { title: 'Javascript: CookBook'},
+  ]
+}
